5f5ebd1c2ac145cbc7485cf1f8da5880d78a6614
Oriol Noya <uriyeahh@gmail.com>
2026-02-10 18:51:37 +0100

    Refactor section retrieval in TranslationsController for improved clarity
    - Updated the method of retrieving all sections from Craft::$app->sections->getAllSections() to Craft::$app->get('sections')->getAllSections() to enhance code readability and maintainability.

--- src/controllers/TranslationsController.php
+++ src/controllers/TranslationsController.php
@@ -161,7 +161,7 @@ class TranslationsController extends Controller
         $offset = ($page - 1) * $perPage;
         $pageRows = array_slice($rows, $offset, $perPage);
 
-        $sections = Craft::$app->sections->getAllSections();
+        $sections = Craft::$app->get('sections')->getAllSections();
         $fieldOptions = $this->getEntryFieldOptions();
 
         return $this->renderTemplate('pragmatic-translations/entradas', [
